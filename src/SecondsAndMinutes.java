a33dbb78e2d84c17828994f127e2837f9961a846
ianktoo <ianktoo@gmail.com>
2025-02-04 15:02:49 -0800

    Print years and days

--- src/SecondsAndMinutes.java
+++ src/SecondsAndMinutes.java
@@ -1,8 +1,9 @@
 public class SecondsAndMinutes {
 
     public static void main(String[] args) {
-        System.out.println(getDurationString(3945));
-        System.out.println(getDurationString(600,59));
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
     }
 
     /*
@@ -10,7 +11,7 @@ public class SecondsAndMinutes {
     1 m - 60 seconds
      */
     public static String getDurationString(int seconds){
-        int hours = 0, minutes = 0, remSeconds = 0;
+        int hours = 0, minutes, remSeconds = 0;
 
         if(seconds < 0){
             return "Invalid seconds.";
@@ -30,11 +31,37 @@ public class SecondsAndMinutes {
 
         // Run a check on invalid seconds and minutes
         if(minutes < 0 || seconds < 0 || seconds >= 60){
-            return "Invalid number time duration.";
+            return "Invalid number time duration. Time must be positive. Seconds must be > 0 but less than 60.";
         }
 
         int totalSeconds = seconds + (minutes * 60);
 
         return getDurationString(totalSeconds);
+    }
+
+
+    public static void printYearsAndDays(long minutes){
+
+        long hours, days, years, remainingDays;
+
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+
+            // calculate number of hours
+            hours = minutes / 60;
+
+            // Calculate days
+            days = hours / 24;
+
+            // calculate years
+            years = days / 365;
+
+            // calculate remaining days
+            remainingDays = days % 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+
+        }
     }
 }
