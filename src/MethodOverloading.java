339767fc8fa506dc279f30e47404ad597a9faf57
ianktoo <ianktoo@gmail.com>
2025-02-04 10:04:09 -0800

    Convert to cm method

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,20 +1,48 @@
 public class MethodOverloading {
-
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        int newScore = calculateScore("Ian Too", 500);
-        System.out.println("new score is: " + newScore);
-
-        int unnamedScore = calculateScore(700);
-        System.out.println("unnamed new score is: " + unnamedScore);
+        System.out.println(calculateScore("Jace",78));
+        System.out.println(convertToCentimeters(6,0));
     }
 
+    /**
+     * Calculates player score.
+     * @param playerName
+     * @param score
+     * @return
+     */
     public static int calculateScore(String playerName, int score) {
         System.out.println(playerName + " score is " + score);
         return score * 1000;
     }
 
+    /**
+     * Calculates anonymous player score.
+     * @param score
+     * @return
+     */
     public static int calculateScore(int score) {
-        System.out.println("Unnamed player score is: " + score);
-        return score * 1000;
+        return calculateScore("Anonymous", score);
+    }
+
+    /**
+     * Returns no player score
+     * @return
+     */
+    public static int calculateScore() {
+        return calculateScore("Anonymous", 0);
+    }
+
+    // 1 inch - 2.54 centimetres
+    // 1 foot - 12 inches
+    public static double convertToCentimeters(int inches) {
+        return (double) inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        return convertToCentimeters(inches) + ((double) feet * 12 * 2.54);
     }
 }
