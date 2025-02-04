5b0bbd7831c62cffb7d4b6e23072ad8584e1ffc6
ianktoo <ianktoo@gmail.com>
2025-02-03 19:14:56 -0800

    Do barking exercise

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -16,10 +16,6 @@ public class BarkingDog {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
          if(hourOfDay < 0 || hourOfDay > 23) {
              return false;
-         } else if((hourOfDay < 8 || hourOfDay > 22) && barking) {
-             return true;
-         } else {
-             return false;
-         }
+         } else return (hourOfDay < 8 || hourOfDay > 22) && barking;
     }
 }
