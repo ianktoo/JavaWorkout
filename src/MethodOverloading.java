5477cba200513ef21f1b7ba54a4adb6714749ec5
ianktoo <ianktoo@gmail.com>
2025-02-04 10:05:14 -0800

    Convert to cm method

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,37 +1,19 @@
 public class MethodOverloading {
-    /**
-     *
-     * @param args
-     */
+
     public static void main(String[] args) {
         System.out.println(calculateScore("Jace",78));
         System.out.println(convertToCentimeters(6,0));
     }
 
-    /**
-     * Calculates player score.
-     * @param playerName
-     * @param score
-     * @return
-     */
     public static int calculateScore(String playerName, int score) {
         System.out.println(playerName + " score is " + score);
         return score * 1000;
     }
 
-    /**
-     * Calculates anonymous player score.
-     * @param score
-     * @return
-     */
     public static int calculateScore(int score) {
         return calculateScore("Anonymous", score);
     }
 
-    /**
-     * Returns no player score
-     * @return
-     */
     public static int calculateScore() {
         return calculateScore("Anonymous", 0);
     }
