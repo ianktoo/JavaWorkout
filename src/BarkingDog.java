1b27dd0ab0de7f471c2be0db92c5445bd66b3993
ianktoo <ianktoo@gmail.com>
2025-02-03 19:13:58 -0800

    Do barking exercise

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,2 +1,25 @@
 public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println("shouldWakeUp(true, 1): " + shouldWakeUp(true, 1));
+        System.out.println("shouldWakeUp(false, 2): " + shouldWakeUp(false, 2));
+        System.out.println("shouldWakeUp(true, 8): " + shouldWakeUp(true, 8));
+        System.out.println("shouldWakeUp(true, -1): " + shouldWakeUp(true, -1));
+
+    }
+
+    /**
+     * There is a dog that likes to bark.
+     * We need to wake up if the dog barks at nignt
+     *
+     */
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+         if(hourOfDay < 0 || hourOfDay > 23) {
+             return false;
+         } else if((hourOfDay < 8 || hourOfDay > 22) && barking) {
+             return true;
+         } else {
+             return false;
+         }
+    }
 }
